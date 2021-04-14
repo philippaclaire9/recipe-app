5695dbdab5beef577e7c4a257d26c6e1389ab845
Philippa Bushell <philippabushell@Philippas-MacBook-Pro.local>
2021-04-14 12:57:14 +0100

    add App.js

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import { searchRecipe } from './api';
+const App = () => {
+  const [text, onChangeText] = useState('write here');
+  const [recipes, setRecipes] = useState([]);
+  const onPress = () => {
+    //console.log('pressed!');
+    searchRecipe(text).then((receivedRecipes) => {
+      console.log(receivedRecipes);
+      setRecipes(receivedRecipes);
+      //console.log(recipes);
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcooooome</Text>
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={onChangeText}
+      />
+      <TouchableHighlight onPress={onPress}>
+        <View style={styles.button}>
+          <Text>Press Here</Text>
+        </View>
+      </TouchableHighlight>
+      {/* {recipes ? (
+        recipes.forEach((recipe) => {
+          <Text>{recipe.title}</Text>;
+        })
+      ) : (
+        <Text>Search for recipes</Text>
+      )} */}
       <StatusBar style="auto" />
     </View>
   );
-}
-
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +51,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
 });
+export default App;
